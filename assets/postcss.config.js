1a50fecd431063b45ad91581902bb12a9dfecba3
Jim <jimfreeze@gmail.com>
2020-03-25 23:43:54 -0500

    changes needed for postcss-import

--- assets/postcss.config.js
+++ assets/postcss.config.js
@@ -38,6 +38,7 @@ const tailwindcss = require('tailwindcss');
 
 module.exports = {
   plugins: [
+    require('postcss-import'),
     tailwindcss('./tailwind.config.js'),
     require('autoprefixer'),
     ...(process.argv.includes("production") && process.argv.includes("--mode"))
